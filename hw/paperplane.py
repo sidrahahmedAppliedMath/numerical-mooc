044f0232ce2295e33ede8081e9de5b5328cb19c2
ikursakov <i.kursakov@gmail.com>
2014-09-29 18:20:36 +0400

    оптимизировали дальность с импользованием optimize.brute

--- hw/paperplane.py
+++ hw/paperplane.py
@@ -8,6 +8,7 @@ Created on Thu Sep 11 23:35:36 2014
 from math import sin, cos, log, ceil, pi
 import numpy
 import matplotlib.pyplot as plt
+from scipy import optimize
 #%matplotlib inline
 from matplotlib import rcParams
 rcParams['font.family'] = 'serif'
@@ -92,7 +93,6 @@ def time_loop(u0,dt):
     u : array of u arrays with shape (n,4), n - number of time steps while y >0
     T : float
         final time
-
     """
     
     u = numpy.zeros((1, 4))
@@ -128,7 +128,6 @@ def time_loop_for_cycle(u0,dt):
     u : array of float
     T : float
         final time
-
     """
     u = numpy.copy(u0) # fill 1st element with initial values
     # time loop - Euler method
@@ -145,7 +144,41 @@ def time_loop_for_cycle(u0,dt):
         T+=dt
     
     return u,T
+def distance(v0_theta0, *params):
+    """Returns the distance that paperplane flies before hitting the ground
+    when launched at given theta_0
     
+    Parameters
+    ----------
+    u0 : array of float
+         initial conditions
+    
+    Returns
+    -------
+    l : float
+        final distance
+    """
+    v0,theta0 = v0_theta0
+    x0,y0,dt = params
+    
+    u,T = time_loop_for_cycle(numpy.array([v0, theta0, x0, y0]),dt)
+#    if u[2]<= 0:
+#        print u[2]
+#        dt = 0.1*dt
+#        u,T = time_loop_for_cycle(numpy.array([v0, theta0, x0, y0]),dt)
+#        print u[2]
+    return u[2]
+def inv_distance(v0_theta0, *params):
+    v0,theta0 = v0_theta0
+    x0,y0,dt = params
+    return 1./distance(v0_theta0, *params)    
+def neg_distance(v0_theta0, *params):
+    v0,theta0 = v0_theta0
+    x0,y0,dt = params
+    return -distance(v0_theta0, *params)    
+
+
+dt = 0.001
 # model parameters for paperplane:
 g = 9.8      # gravity in m s^{-2}
 v_t = 4.9   # trim velocity in m s^{-1}   
@@ -157,7 +190,7 @@ C_L = 1.0    # for convenience, use C_L = 1
 x0 = 0.0  # horizotal position is arbitrary
 y0 = 1.5  # initial altitude
 
-v0_max = 10.5 #maximum initial velocity
+v0_max = 12.5 #maximum initial velocity
 thetha0_max = pi/3 #maximal initial angle
 
 dv0 = 0.5 #velocity step
@@ -167,59 +200,70 @@ N_v0 = int(v0_max/dv0) + 1
 N_t0 = int(thetha0_max/dt0 ) + 1 
 
 max_distances = numpy.zeros((N_v0,N_t0))
-#ini_values = numpy.array([[i,j] for i in numpy.linspace(0.5,v0_max,N_v0) 
-#for j in numpy.linspace(0,thetha0_max,N_t0)])
-#
-#
-#triples = numpy.empty_like(ini_values,dtype=numpy.ndarray)
-#for i,v0_theta0 in enumerate(ini_values):
+
 v0_values = numpy.linspace(0.5,v0_max,N_v0) 
 theta0_values = numpy.linspace(-0.5,thetha0_max,N_t0)
-for i in range(N_v0):
-    v0 = v0_values[i]
-    for j in range(N_t0):
-        theta0 = theta0_values[j]
-        
-        dt = 0.001    
-        u,T = time_loop_for_cycle(numpy.array([v0, theta0, x0, y0]),dt)
-        max_distances[i,j]=  u[2]
+
+rranges = (slice(0.5,v0_max,dv0),slice(-0.5,thetha0_max,dt0))
+resbrute = optimize.brute(neg_distance, rranges, args=(x0,y0,dt), full_output=True,
+                          finish=optimize.fmin)
+
+resbrute_none = optimize.brute(neg_distance, rranges, args=(x0,y0,dt), full_output=True,
+                          finish=None)
+
+#==============================================================================
+# 
+# for i in range(N_v0):
+#     v0 = v0_values[i]
+#     for j in range(N_t0):
+#         theta0 = theta0_values[j]
+#         
+#         dt = 0.001    
+# #        u,T = time_loop_for_cycle(numpy.array([v0, theta0, x0, y0]),dt)
+# #        max_distances[i,j]=  u[2]
+# #        u,T = time_loop_for_cycle(numpy.array([v0, theta0, x0, y0]),dt)
+#         max_distances[i,j]=  distance_theta0_const(v0)
+# 
+# 
+# max_dist =  max_distances.max()    
+# max_ind = numpy.where( max_distances == max_dist )
+# max_ind = zip(max_ind[0],max_ind[1])[0]
+# v0 = v0_values[max_ind[0]]
+# 
+# theta0 = theta0_values[max_ind[1]] 
+# 
+# print "Maximum distance is %.2f. \
+# Initial values: v0 = %.2f, t0 = %.2f" % (max_dist,v0,theta0)
+# 
+# 
+# 
+# dt = 0.001    
+# u,T = time_loop(numpy.array([v0, theta0, x0, y0]),dt)
+# # get the glider's position with respect to the time
+# x = u[:,2]
+# y = u[:,3]
+# v = u[:,0]
+# 
+# # visualization of the path
+# plt.figure(figsize=(8,6))
+# plt.grid(True)
+# plt.xlabel(r'x', fontsize=18)
+# plt.ylabel(r'y', fontsize=18)
+# plt.title('Glider trajectory, flight time = %.2f , distance = %.2f' % (T,max(x)), fontsize=18)
+# plt.plot(x,y, 'k-', lw=2);
+# 
+# plt.figure(figsize=(8,6))
+# plt.pcolor(max_distances)
+# plt.colorbar()
+# plt.ylabel(r'$v_0$', fontsize=18)
+# ylocs,ylabels = plt.yticks()
+# plt.yticks(ylocs[:-1],tuple('%.2f' % v0_values[int(i)] for i in ylocs[:-1]))
+# plt.xlabel(r'$\theta_0$', fontsize=18)
+# xlocs,xlabels = plt.xticks()
+# plt.xticks(xlocs[:-1],tuple('%.2f' % theta0_values[int(i)] for i in xlocs[:-1]))
+# plt.suptitle('Maximum distance (in m) for different initial conditions.', fontsize=18)
+#plt.show()
+#==============================================================================
 
 
-max_dist =  max_distances.max()    
-max_ind = numpy.where( max_distances == max_dist )
-max_ind = zip(max_ind[0],max_ind[1])[0]
-v0 = v0_values[max_ind[0]]
 
-theta0 = theta0_values[max_ind[1]] 
-
-print "Maximum distance is %.2f. \
-Initial values: v0 = %.2f, t0 = %.2f" % (max_dist,v0,theta0)
-
-
-
-dt = 0.001    
-u,T = time_loop(numpy.array([v0, theta0, x0, y0]),dt)
-# get the glider's position with respect to the time
-x = u[:,2]
-y = u[:,3]
-v = u[:,0]
-
-# visualization of the path
-plt.figure(figsize=(8,6))
-plt.grid(True)
-plt.xlabel(r'x', fontsize=18)
-plt.ylabel(r'y', fontsize=18)
-plt.title('Glider trajectory, flight time = %.2f , distance = %.2f' % (T,max(x)), fontsize=18)
-plt.plot(x,y, 'k-', lw=2);
-
-plt.figure(figsize=(8,6))
-plt.pcolor(max_distances)
-plt.colorbar()
-plt.ylabel(r'$v_0$', fontsize=18)
-ylocs,ylabels = plt.yticks()
-plt.yticks(ylocs[:-1],tuple('%.2f' % v0_values[int(i)] for i in ylocs[:-1]))
-plt.xlabel(r'$\theta_0$', fontsize=18)
-xlocs,xlabels = plt.xticks()
-plt.xticks(xlocs[:-1],tuple('%.2f' % theta0_values[int(i)] for i in xlocs[:-1]))
-plt.suptitle('Maximum distance (in m) for different initial conditions.', fontsize=18)
-plt.show()
